39a9b328cfb0f92b1eafb95a7f3b3f14ee34c5bc
tulsivangari05 <152706311+tulsivangari05@users.noreply.github.com>
2024-12-17 09:40:43 +0530

    Add files via upload

---
+++ public.js
@@ -0,0 +1,34 @@
+// const eating= document.querySelectorAll(".eating")
+// var counter= 0;
+
+// console.log(eating);
+
+
+
+// eating.forEach(
+//     (eating,index) => {
+//         eating.style.left= '${index * 100}%'
+//     }
+// );
+
+// const goPrev = () => {
+//     counter--
+//     eatingImage()
+// }
+// const goNext = () => {
+//     counter++
+//     eatingImage()
+// }
+
+// const eatingImage = () => {
+//     eating.forEach(
+//         (eating) => {
+//             slide.style.transform = 'translateX(-${counter * 100}%)'
+//         }
+//     )
+// }
+
+
+
+
+
